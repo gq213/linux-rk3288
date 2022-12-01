cf5ce9b46fba68560616a9447d9c5d90fd5ebe7a
gq213 <gaoqiang1211@gmail.com>
2022-12-01 11:42:05 +0800

    enable uvc.

--- drivers/usb/dwc2/params.c
+++ drivers/usb/dwc2/params.c
@@ -142,6 +142,7 @@ static void dwc2_set_rk_params(struct dwc2_hsotg *hsotg)
 	p->host_perio_tx_fifo_size = 256;
 	p->ahbcfg = GAHBCFG_HBSTLEN_INCR16 <<
 		GAHBCFG_HBSTLEN_SHIFT;
+	p->lpm = false;
 	p->power_down = DWC2_POWER_DOWN_PARAM_NONE;
 }
 
